24ee362dd59713108ae80ac2d2ff8269474315b3
karatehcops <contact@lazers.tv>
2019-07-05 12:27:52 -0400

    Add 404 page unit test.

---
+++ src/pages/404/__tests__/404.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { useStaticQuery } from 'gatsby';
+import NotFoundPage from '../index';
+
+beforeEach(() => {
+  useStaticQuery.mockReturnValueOnce({
+    site: {
+      siteMetadata: {
+        title: 'My site',
+        author: 'Tester',
+      },
+    },
+  });
+});
+
+describe('NotFoundPage', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <NotFoundPage
+          data={{
+            site: {
+              siteMetadata: {
+                title: 'My Test Site',
+              },
+            },
+          }}
+        />,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
