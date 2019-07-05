de64f1b0222d16106485647dfa0942911c98222c
karatehcops <contact@lazers.tv>
2019-07-05 13:11:58 -0400

    Add named imports to template.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,8 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import { Heading, Paragraph } from 'grommet';
-import Layout from '../components/Layout';
-import SEO from '../components/Seo';
+import { Layout, SEO } from '../components';
 
 class BlogPostTemplate extends React.Component {
   render() {
