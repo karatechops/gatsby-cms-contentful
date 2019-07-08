6a6b9ffebbc46daa592bbc6482e0a5b1517c931e
karatehcops <contact@lazers.tv>
2019-07-08 09:55:52 -0400

    Add dynamic title to nav.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { useStaticQuery, graphql, Link } from 'gatsby';
 import { Box, Text } from 'grommet';
 
-export const Header = () => (
-  <Box
-    direction="row"
-    background="dark-1"
-    gap="small"
-    pad={{ vertical: 'small', horizontal: 'medium' }}
-    justify="between"
-  >
-    <Link to="/">
-      <Text weight="bold" color="accent-1">
-        Home
-      </Text>
-    </Link>
-    <Link to="/blog">
-      <Text weight="bold" color="accent-1">
-        Blog
-      </Text>
-    </Link>
-  </Box>
-);
+function Header() {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `,
+  );
 
+  return (
+    <Box
+      direction="row"
+      background="dark-1"
+      gap="small"
+      pad={{ vertical: 'small', horizontal: 'medium' }}
+      justify="between"
+    >
+      <Link to="/">
+        <Text weight="bold" color="accent-1">
+          {(site.siteMetadata && site.siteMetadata.title) || 'Home'}
+        </Text>
+      </Link>
+      <Link to="/blog">
+        <Text weight="bold" color="accent-1">
+          Blog
+        </Text>
+      </Link>
+    </Box>
+  );
+}
 export default Header;
