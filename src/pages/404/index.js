76e405d0ffc7fe67930152162e11bac64a628ca0
karatehcops <contact@lazers.tv>
2019-07-09 11:07:29 -0400

    Fix 404 page.

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -3,21 +3,17 @@ import { Box, Heading, Paragraph } from 'grommet';
 import { Layout, SEO } from '../../components';
 import { useSiteMetadata } from '../../hooks/use-site-metadata';
 
-class NotFoundPage extends React.Component {
-  render() {
-    const siteMetadata = useSiteMetadata();
-    return (
-      <Layout title={siteMetadata.siteTitle}>
-        <Box flex align="center" justify="center">
-          <SEO title="404: Not Found" />
-          <Heading>Not Found</Heading>
-          <Paragraph>
-            You just hit a route that doesn&#39;t exist... the sadness.
-          </Paragraph>
-        </Box>
-      </Layout>
-    );
-  }
+function NotFoundPage() {
+  const siteMetadata = useSiteMetadata();
+  return (
+    <Layout title={siteMetadata.siteTitle}>
+      <Box flex align="center" justify="center">
+        <SEO title="404: Not Found" />
+        <Heading>Not Found</Heading>
+        <Paragraph>This page doesn&#39;t exist... the sadness.</Paragraph>
+      </Box>
+    </Layout>
+  );
 }
 
 export default NotFoundPage;
